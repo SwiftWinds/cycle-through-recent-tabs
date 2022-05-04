9115bcfe5b2a664e74dca175f4c2506552611778
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-05-03 17:18:07 -0700

    Add comment and reformat

--- background.js
+++ background.js
@@ -51,6 +51,8 @@ const main = async () => {
     const tabToRemove = { tabId, windowId };
     const [, curTab] = findCurrentTab();
     if (equals(curTab, tabToRemove)) {
+      // if the tab removed was the current tab, we wait for onActivated
+      // listener to finish to avoid race conditions
       activationPromise = new Promise((resolve) => {
         resolveActivationPromise = resolve;
       });
@@ -64,35 +66,34 @@ const main = async () => {
   });
 
   // adds tab to recentTabs on tab change
-  browser.tabs.onActivated.addListener(
-    ({ tabId, windowId }) => {
-      const tabToAdd = { tabId, windowId, lastSeen: 0 };
-      if (isTraversingHistory) {
-        return;
-      }
-      const [curIdx] = findCurrentTab();
-      if (curIdx !== recentTabs.length - 1) {
-        recentTabs.length = curIdx + 1;
-      }
+  browser.tabs.onActivated.addListener(({ tabId, windowId }) => {
+    const tabToAdd = { tabId, windowId, lastSeen: 0 };
+    if (isTraversingHistory) {
+      return;
+    }
+    const [curIdx] = findCurrentTab();
+    if (curIdx !== recentTabs.length - 1) {
+      recentTabs.length = curIdx + 1;
+    }
 
-      // we want to remove any instances of the newly activate tab from our
-      // array because we'll append it to the end of the array later with
-      // lastSeen = 0
-      // (we cannot do binary search because it's sorted by lastSeen, not tabId
-      //  or windowId)
-      recentTabs = recentTabs.filter((tab) => !equals(tab, tabToAdd));
+    // we want to remove any instances of the newly activate tab from our
+    // array because we'll append it to the end of the array later with
+    // lastSeen = 0
+    // (we cannot do binary search because it's sorted by lastSeen, not tabId
+    //  or windowId)
+    recentTabs = recentTabs.filter((tab) => !equals(tab, tabToAdd));
 
-      for (const tab of recentTabs) {
-        tab.lastSeen++;
-      }
+    for (const tab of recentTabs) {
+      tab.lastSeen++;
+    }
 
-      recentTabs.push(tabToAdd);
+    recentTabs.push(tabToAdd);
 
-      if (activationPromise) {
-        resolveActivationPromise();
-        activationPromise = null;
-      }
-    });
+    if (activationPromise) {
+      resolveActivationPromise();
+      activationPromise = null;
+    }
+  });
 
   browser.commands.onCommand.addListener(async (command) => {
     isTraversingHistory = true;
