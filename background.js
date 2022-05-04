0b9beaaed59f16b6180dd9c6d43cc8e2916c2018
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-05-03 17:00:25 -0700

    Don't go to same tab twice when going back in time

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
+let isTraversingHistory = false;
+let recentTabs = [];
+
+// we wait for onActivated listener to finish before executing the onRemoved listener
+// when the tab removed was the current tab
+let activationPromise;
+let resolveActivationPromise;
+
 const getCurrentTab = async () => {
   const tabs = await browser.tabs.query({
     active: true,
@@ -6,72 +14,84 @@ const getCurrentTab = async () => {
   return browser.tabs.get(tabs[0].id);
 };
 
-function* pairwise(array) {
-  for (let baseIdx = 0; baseIdx <= array.length - 2; baseIdx++) {
-    console.log(baseIdx, baseIdx + 2);
-    yield array.slice(baseIdx, baseIdx + 2);
+// binary search for current tab in recentTabs
+// (current tab is the one where lastSeen === 0)
+const findCurrentTab = () => {
+  let start = 0;
+  let end = recentTabs.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (recentTabs[mid].lastSeen === 0) {
+      return [mid, recentTabs[mid]];
+    }
+
+    if (recentTabs[mid].lastSeen < 0) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
   }
-}
+  return [-1, null];
+};
+
+const equals = (a, b) => a.tabId === b.tabId && a.windowId === b.windowId;
 
 const main = async () => {
-  let isTraversingHistory = false;
-  let idx = 0;
-  let recentTabs = [];
-
-  // we wait for onActivated listener to finish before executing the onRemoved listener
-  // when the tab removed was the current tab
-  let activationPromise;
-  let resolveActivationPromise;
 
   // store current tab in recentTabs
   const { id: tabId, windowId } = await getCurrentTab();
-  recentTabs.push({ tabId, windowId });
+  recentTabs.push({ tabId, windowId, lastSeen: 0 });
 
   // removes all instances of closed tab from recentTabs
   browser.tabs.onRemoved.addListener(async (tabId, { windowId }) => {
-    if (tabId === recentTabs[idx].tabId) {
+    const tabToRemove = { tabId, windowId };
+    const [, curTab] = findCurrentTab();
+    if (equals(curTab, tabToRemove)) {
       activationPromise = new Promise(function(resolve) {
         resolveActivationPromise = resolve;
       });
       await activationPromise;
     }
 
-    // we use a count because modifying idx in the for loop will mess with the
-    // if statement
-    let count = 0;
-    for (const [i, tab] of recentTabs.entries()) {
-      // i can never === idx because we wait for onActivated listener to finish
-      // before executing the onRemoved listener
-      if (tab.tabId === tabId && tab.windowId === windowId && i < idx) {
-        count++;
-      }
-    }
-    recentTabs = recentTabs
-      .filter(tab => tab.tabId !== tabId || tab.windowId !== windowId); // remove closed tabs
-    idx -= count;
+    // we remove all instances of closed tab from recentTabs
+    // (we cannot do binary search because it's sorted by lastSeen, not tabId
+    //  or windowId)
+    recentTabs = recentTabs.filter((tab) => !equals(tab, tabToRemove));
 
-    for (const [prevTab, curTab] of pairwise(recentTabs)) {
-      if (prevTab.tabId === curTab.tabId && prevTab.windowId === curTab.windowId) {
-        idx--;
-      }
-    }
-    recentTabs = recentTabs.filter((curTab, pos, arr) => { // remove consecutive duplicates
-      const prevTab = arr[pos - 1];
-      // Always keep the 0th element as there is nothing before it
-      // Then check if each element is different from the one before it
-      return pos === 0 ||
-        (prevTab.tabId !== curTab.tabId || prevTab.windowId !== curTab.windowId);
-    });
+    console.log("onRemoved:", { recentTabs });
   });
 
   // adds tab to recentTabs on tab change
   browser.tabs.onActivated.addListener(
     async ({ tabId, windowId }) => {
-      if (!isTraversingHistory) {
-        recentTabs.length = idx + 1;
-        recentTabs.push({ tabId, windowId });
-        idx++;
+      if (windowId !== 3) {
+        return;
       }
+      const tabToAdd = { tabId, windowId, lastSeen: 0 };
+      if (isTraversingHistory) {
+        return;
+      }
+      const [curIdx] = findCurrentTab();
+      if (curIdx !== recentTabs.length - 1) {
+        recentTabs.length = curIdx + 1;
+      }
+
+      // we want to remove any instances of the newly activate tab from our
+      // array because we'll append it to the end of the array later with
+      // lastSeen = 0
+      // (we cannot do binary search because it's sorted by lastSeen, not tabId
+      //  or windowId)
+      recentTabs = recentTabs.filter((tab) => !equals(tab, tabToAdd));
+
+      for (const tab of recentTabs) {
+        tab.lastSeen++;
+      }
+
+      recentTabs.push(tabToAdd);
+
+      console.log("onActivated:", { recentTabs });
 
       if (activationPromise) {
         resolveActivationPromise();
@@ -81,15 +101,26 @@ const main = async () => {
 
   browser.commands.onCommand.addListener(async (command) => {
     isTraversingHistory = true;
-    if (command === "go-back" && idx > 0) {
-      idx--;
-    } else if (command === "go-forward" && idx < recentTabs.length - 1) {
-      idx++;
+    const [curIdx] = findCurrentTab();
+    let newIdx;
+    if (command === "go-back" && curIdx > 0) {
+      newIdx = curIdx - 1;
+    } else if (command === "go-forward" && curIdx < recentTabs.length - 1) {
+      newIdx = curIdx + 1;
     }
-    await Promise.all([
-      browser.windows.update(recentTabs[idx].windowId, { focused: true }),
-      browser.tabs.update(recentTabs[idx].tabId, { active: true }),
-    ]);
+    if (newIdx !== undefined) {
+      const prevLastSeen = recentTabs[newIdx].lastSeen;
+      for (const tab of recentTabs) {
+        tab.lastSeen -= prevLastSeen;
+      }
+      await Promise.all([
+        browser.windows.update(recentTabs[newIdx].windowId, { focused: true }),
+        browser.tabs.update(recentTabs[newIdx].tabId, { active: true }),
+      ]);
+    }
+
+    console.log("onCommand:", { recentTabs });
+
     isTraversingHistory = false;
   });
 };
